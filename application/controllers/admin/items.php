d223425979eb0d0c30f37bc87e5d259d42dbcc42
Rob Meijer <rob.meijer@worldstores.co.uk>
2012-06-15 16:06:25 +0100

    Update items cache to use remember() function.

--- application/controllers/admin/items.php
+++ application/controllers/admin/items.php
@@ -23,16 +23,12 @@ class Admin_Items_Controller extends Admin_Controller {
 		$extra_pages = 4;
 		$start_total = $num_pages * $per_page > $count ? $count : $num_pages * $per_page;
 		$total = $page * $per_page < $start_total - $per_page * $extra_pages ? $start_total : (($page + $extra_pages) * $per_page > $count ? $count : ($page + $extra_pages) * $per_page);
-		$cache = 'items_'.$page.'_'.$per_page;
-		if (Cache::has($cache))
-		{
-			$items = Cache::get($cache);
-		}
-		else
-		{
-			$items = Item::take($per_page)->skip(($page-1)*$per_page)->get();
-			Cache::put($cache, $items, 60);
-		}
+		
+		$items_cache = 'items_'.$page.'_'.$per_page;
+		$items = Cache::remember($items_cache, function() use ($page, $per_page) {
+			return Item::take($per_page)->skip(($page - 1) * $per_page)->get();
+		}, 30);
+
 		$items = Paginator::make($items, $total, $per_page);
 		$this->layout->page_title = "Admin - All items";
 		$this->layout->page_content =
